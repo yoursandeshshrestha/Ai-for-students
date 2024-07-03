f8b6cfd806b8dd3b780607929ac42a533cf47b99
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-03 22:00:33 +0530

    all code pushed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/Main";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
